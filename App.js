b493280313877ab5e41e0dd12b00435e0ba4416a
Yash Patni <yash.s.patni@gmail.com>
2022-09-23 13:13:37 +0530

    [Yash] Used native custom module in js application for showing toast and deviceId

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View} from 'react-native';
+import {CustomModule} from './CustomModule';
 
 const App = () => {
+  const [deviceId, setDeviceId] = useState('');
+
+  useEffect(() => {
+    const fetchDeviceId = async () => {
+      const id = await CustomModule.getDeviceId();
+      setDeviceId(id);
+    };
+
+    fetchDeviceId();
+  }, []);
+
+  useEffect(() => {
+    CustomModule.showToast();
+  }, []);
+
   return (
     <View>
       <Text>Hello World!</Text>
+      <Text>Device id: {deviceId}</Text>
     </View>
   );
 };
